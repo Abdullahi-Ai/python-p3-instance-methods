30412bfed33f10fc736f4f2e9f7747aee1266ab5
Abdullahi-Ai <abdullahiibrashair@gmail.com>
2025-05-11 11:25:24 +0300

    python-p3-instance-methods

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+  # Class body goes here
+
 
 class Person:
-    # Class body goes here
-
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+Ali = Person()
+Ali.talk()
+Ali.walk()
